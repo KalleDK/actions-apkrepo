7dd891df5587b7f9224ae9f756102a365c573e6e
Kalle R. Aagaard <git@k-moeller.dk>
2022-12-28 23:19:35 +0100

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,26 +5,13 @@ set -e
 echo "::group::Setup"
 
 export PACKAGER="${INPUT_ABUILD_PACKAGER}"
-export SRCDEST="${GITHUB_WORKSPACE}/cache/distfiles"
-export REPODEST="${GITHUB_WORKSPACE}/packages"
-export SUBREPO_BUILD="${REPODEST}/workspace"
-export REPONAME=${INPUT_ABUILD_REPO_NAME}
-export SUBREPO="${REPODEST}/${REPONAME}"
-export BASE_URL=${INPUT_ABUILD_REPO_URL}
-
-export PREFIX=${INPUT_ABUILD_PREFIX:-.}
-export GIT_COMMIT=${INPUT_ABUILD_PKG_COMMIT}
-export PKG_VER=${INPUT_ABUILD_PKG_VER}
-export ABUILD_DIR=~/.abuild
-
-
-ls -la
-ls -la dist
 
 apk index -o ./dist/APKINDEX.tar.gz ./dist/*.apk
-abuild-sign -k $(pwd)/apk.rsa ./dist/APKINDEX.tar.gz
-
-ls -la
-ls -la dist
-
+abuild-sign -k $(pwd)/${INPUT_ABUILD_KEY_NAME}.rsa ./dist/APKINDEX.tar.gz
+mkdir ./apk-repo
+cp ${INPUT_ABUILD_KEY_NAME}.rsa.pub ./apk-repo/
+cp ./dist/*.apk ./apk-repo/
+cp ./dist/APKINDEX.tar.gz ./apk-repo/
+ls -la ./apk-repo
+echo "::set-output name=repo_path::./apk-repo"
 echo "::endgroup::"
