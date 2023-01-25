ae10c62d8931b892ffae2017c7db6d15b6b8b43e
Kalle Møller <git@k-moeller.dk>
2023-01-25 12:34:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,43 @@
 #!/usr/bin/env sh
 
-set -e
+set -ex
 
 echo "::group::Setup"
 
 export PACKAGER="${INPUT_ABUILD_PACKAGER}"
-export REPONAME=${INPUT_ABUILD_REPO_NAME}
-export BASE_URL=${INPUT_ABUILD_REPO_URL}
+export BUILD_DIR=./.apk
+export APKREPO="${BUILD_DIR}/repo"
+export APKKEYS="${BUILD_DIR}/keys"
 
-apk index -o ./dist/APKINDEX.tar.gz ./dist/*.apk
-abuild-sign -k $(pwd)/${INPUT_ABUILD_KEY_NAME}.rsa ./dist/APKINDEX.tar.gz
-mkdir -p ./apk/x86_64
-cp ${INPUT_ABUILD_KEY_NAME}.rsa.pub ./apk/
-cp ./dist/*.apk ./apk/x86_64/
-cp ./dist/APKINDEX.tar.gz ./apk/x86_64/
-find ./apk
-cat << EOF > ./apk/index.md
+echo "::group::Setup Base"
+mkdir -p ${APKREPO}
+echo "::endgroup::"
+
+echo "::group::Setup Keys"
+printf -- '${INPUT_ABUILD_KEY_PRIV}' > ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa
+printf -- '${INPUT_ABUILD_KEY_PUB}' > ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa.pub
+cp ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa.pub ${APKREPO}/
+echo "::endgroup::"
+
+echo "::group::Sign x86_64"
+mkdir -p ${APKREPO}/x86_64
+cp ./x86_64/*/*.apk ${APKREPO}/x86_64/
+apk index -o ${APKREPO}/x86_64/APKINDEX.tar.gz ${APKREPO}/x86_64/*.apk
+abuild-sign -k ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa ${APKREPO}/x86_64/APKINDEX.tar.gz
+echo "::endgroup::"
+
+echo "::group::Create index"
+cat << EOF > ${APKREPO}/index.md
 # ACME DNS Proxy
 
 \`\`\`bash
 # Install key
-wget -O "/etc/apk/keys/${INPUT_ABUILD_KEY_NAME}.rsa.pub" "${BASE_URL}/${REPONAME}/apk/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
+wget -O "/etc/apk/keys/${INPUT_ABUILD_KEY_NAME}.rsa.pub" "${INPUT_ABUILD_REPO_URL}/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
 
 # Install repo
-echo "${BASE_URL}/${REPONAME}/apk" >> /etc/apk/repositories
+echo "${INPUT_ABUILD_REPO_URL}" >> /etc/apk/repositories
 \`\`\` 
 EOF
-
-cat ./apk/index.md
-echo "repo_path=./apk" >> $GITHUB_OUTPUT
 echo "::endgroup::"
+
+echo "repo_path=${APKREPO}" >> $GITHUB_OUTPUT
