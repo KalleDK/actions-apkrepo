3b645e6f2f60be38fd4fced5d0f1ef14403129f1
Kalle Møller <git@k-moeller.dk>
2023-01-25 12:48:05 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,18 @@ export BUILD_DIR=./.apk
 export APKREPO="${BUILD_DIR}/repo"
 export APKKEYS="${BUILD_DIR}/keys"
 
-echo "::group::Setup Base"
-mkdir -p ${APKREPO}
+echo "::group::Setup Build Dir"
+mkdir -p ${BUILD_DIR}
 echo "::endgroup::"
 
 echo "::group::Setup Keys"
+mkdir -p ${APKKEYS}
 printf -- '${INPUT_ABUILD_KEY_PRIV}' > ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa
 printf -- '${INPUT_ABUILD_KEY_PUB}' > ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa.pub
-cp ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa.pub ${APKREPO}/
+echo "::endgroup::"
+
+echo "::group::Setup Repo"
+mkdir -p ${APKREPO}
 echo "::endgroup::"
 
 echo "::group::Sign x86_64"
@@ -24,6 +28,10 @@ mkdir -p ${APKREPO}/x86_64
 cp ./x86_64/*/*.apk ${APKREPO}/x86_64/
 apk index -o ${APKREPO}/x86_64/APKINDEX.tar.gz ${APKREPO}/x86_64/*.apk
 abuild-sign -k ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa ${APKREPO}/x86_64/APKINDEX.tar.gz
+echo "::endgroup::"
+
+echo "::group::Add public key"
+cp ${APKKEYS}/${INPUT_ABUILD_KEY_NAME}.rsa.pub ${APKREPO}/
 echo "::endgroup::"
 
 echo "::group::Create index"
@@ -38,6 +46,7 @@ wget -O "/etc/apk/keys/${INPUT_ABUILD_KEY_NAME}.rsa.pub" "${INPUT_ABUILD_REPO_UR
 echo "${INPUT_ABUILD_REPO_URL}" >> /etc/apk/repositories
 \`\`\` 
 EOF
+
 echo "::endgroup::"
 
 echo "repo_path=${APKREPO}" >> $GITHUB_OUTPUT
