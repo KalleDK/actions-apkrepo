9b2346efdfb06e65a649884ae7bc5cd98f6eba3a
Kalle Møller <git@k-moeller.dk>
2025-09-18 11:41:57 +0000

    Update

---
+++ .devcontainer/setup_env.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import subprocess
+
+PACKAGER = os.getenv("PACKAGER", "Joe Smith <joe.smith@example.com>")
+
+TMPL = '''export GITHUB_TOKEN=ghp_yourtokenhere
+export INPUT_PKGS_PATH="src"
+export INPUT_ABUILD_REPO_URL="https://example.com/repo"
+export INPUT_ABUILD_PACKAGER="{PACKAGER}"
+export INPUT_ABUILD_KEY_NAME="{KEY_NAME}"
+export INPUT_ABUILD_KEY_PRIV={KEY_PRIV}
+export INPUT_ABUILD_KEY_PUB={KEY_PUB}
+export GITHUB_OUTPUT="./github-output"
+'''
+
+envrc_file = pathlib.Path('.envrc')
+
+if not envrc_file.exists():
+    subprocess.run(['abuild-keygen', '-n'], env={**os.environ, "PACKAGER": PACKAGER}, check=True, capture_output=True)
+    abuild_path = pathlib.Path.home().joinpath('.abuild')
+    priv = next(abuild_path.glob('*.rsa'))
+    pub = priv.with_suffix('.rsa.pub')
+    envrc_file.write_text(TMPL.format(
+        PACKAGER=PACKAGER,
+        KEY_NAME=priv.stem,
+        KEY_PRIV=repr(priv.read_text().strip()),
+        KEY_PUB=repr(pub.read_text().strip()),
+    ))
+    pub.unlink()
+    priv.unlink()
+    abuild_path.rmdir()
+
+subprocess.run(['direnv', 'allow'], check=True)
